8e384802e45561957c1511ee19f1c445831e4bc7
Alex Corey <alex.swansboro@gmail.com>
2018-09-05 13:27:34 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    
+  }
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Calculator</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className='App-intro' />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
